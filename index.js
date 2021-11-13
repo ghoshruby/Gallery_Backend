b3de4539cb4f6a4e517b13a7235a71846e06a983
Ruby Ghosh <60173326+ghoshruby@users.noreply.github.com>
2021-11-13 21:00:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//The express() syntax is the equivalent of saying new express(). 
+//It creates a new instance of express that you can assign to a variable.
+var app = express();
+
+var bodyParser = require("body-parser");
+//A new body object containing the parsed data is populated 
+//on the request object after the middleware (i.e. req.body). 
+//This object will contain key-value pairs, 
+app.use(bodyParser.json());
+
+var cors = require('cors')
+app.use(cors())
+
+const mymongoose  = require('./DatabaseConnect.js')
+const userApi = require('./controller/User_API')
+
+app.use('/user',userApi);
+
+
+
+
+app.listen(4500,()=>console.log('Express Started on 4500'))
